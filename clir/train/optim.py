0fa78ec53b0d37fa489e75e5aa02251255fbf28b
Maxwell1447 <maxbouthors@gmail.com>
2024-04-17 00:12:17 +0200

    changed behavior for isqrt

--- clir/train/optim.py
+++ clir/train/optim.py
@@ -43,14 +43,14 @@ class InverseSqrtLRWithWarmup(LambdaLR):
     def __init__(self, *args, warmup_steps, total_steps, update_factor, **kwargs):
         self.warmup_steps = warmup_steps
         self.total_steps = total_steps
-        self.update_factor = update_factor
+        self.update_factor = update_factor if update_factor is not None else (warmup_steps if warmup_update > 10 else total_steps / 20)
         super().__init__(*args, **kwargs, lr_lambda=self.lr_lambda)
             
     def lr_lambda(self, current_step: int):
         if current_step < self.warmup_steps:
             return float(current_step) / float(max(1, self.warmup_steps))
-        return 1 / np.sqrt(float(current_step - self.warmup_steps) / float(self.total_steps - self.warmup_steps) * self.update_factor ** 2 + 1)
-        # return 1 / np.sqrt(float(current_step - self.warmup_steps) / float(self.update_factor) + 1)
+        # return 1 / np.sqrt(float(current_step - self.warmup_steps) / float(self.total_steps - self.warmup_steps) * self.update_factor ** 2 + 1)
+        return 1 / np.sqrt(float(current_step - self.warmup_steps) / float(self.update_factor) + 1)
 
 class LabelSmoothingLoss(nn.Module):
     """NLL loss with label smoothing.
