4e36c4cdccc20a13061d864653f9177246677144
Maxwell1447 <maxbouthors@gmail.com>
2024-04-16 23:04:57 +0200

    Revert "not working but with new scheduler"
    This reverts commit 130bf790af4ddd1bbbb0b5dd7a4e45d7be9e1d6b.

--- clir/train/optim.py
+++ clir/train/optim.py
@@ -2,7 +2,6 @@
 import torch
 from torch import nn
 from torch.optim.lr_scheduler import LambdaLR
-import numpy as np
 
 
 class LinearLRWithWarmup(LambdaLR):
@@ -29,43 +28,12 @@ class LinearLRWithWarmup(LambdaLR):
             float(self.total_steps - current_step) / float(max(1, self.total_steps - self.warmup_steps))
         )
 
-class InverseSqrtLRWithWarmup(LambdaLR):
-    """
-    Linear learning rate scheduler with linear warmup.
-    Adapted from https://github.com/huggingface/transformers/blob/v4.23.0/src/transformers/optimization.py#L75
-    
-    Parameters
-    ----------
-    *args, **kwargs: additionnal arguments are passed to LambdaLR
-    warmup_steps: int
-    total_steps: int
-    """
-    def __init__(self, *args, warmup_steps, total_steps, update_factor, **kwargs):
-        self.warmup_steps = warmup_steps
-        self.total_steps = total_steps
-        self.update_factor = update_factor
-        super().__init__(*args, **kwargs, lr_lambda=self.lr_lambda)
-            
-    def lr_lambda(self, current_step: int):
-        if current_step < self.warmup_steps:
-            return float(current_step) / float(max(1, self.warmup_steps))
-        return 1 / np.sqrt(float(current_step - self.warmup_steps) / float(self.total_steps - self.warmup_steps) * self.update_factor ** 2 + 1)
-        # return 1 / np.sqrt(float(current_step - self.warmup_steps) / float(self.update_factor) + 1)
 
-class LabelSmoothingLoss(nn.Module):
-    """NLL loss with label smoothing.
-    """
-    def __init__(self, smoothing=0.0):
-        """Constructor for the LabelSmoothing module.
-        :param smoothing: label smoothing factor
-        """
-        super(LabelSmoothingLoss, self).__init__()
-        self.confidence = 1.0 - smoothing
-        self.smoothing = smoothing
-
-    def forward(self, logprobs, target):
-        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
-        nll_loss = nll_loss.squeeze(1)
-        smooth_loss = -logprobs.mean(dim=-1)
-        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
-        return loss.mean()
+# def _calc_mml(loss_tensor):
+#     """Taken from dpr.models.reader to avoid extra-dependency"""
+#     marginal_likelihood = torch.sum(torch.exp(- loss_tensor - 1e10 * (loss_tensor == 0).float()), 1)
+#     # Mean reduction: this is different from https://github.com/facebookresearch/DPR/blob/a31212dc0a54dfa85d8bfa01e1669f149ac832b7/dpr/models/reader.py#L180
+#     # who use sum reduction
+#     # by averaging, the loss does not depend on the batch size N (number of questions)
+#     # it might still depend on M, the number of passages, if `max_pooling=True` in `multi_passage_rc_loss` (not recommanded)
+#     return -torch.mean(torch.log(marginal_likelihood + torch.ones(loss_tensor.size(0), device=marginal_likelihood.device) * (marginal_likelihood == 0).float()))
